ddde733057642db914aed35ecc1c23454396549e
DESKTOP-2JEJU0Q\FeciP <fplutzer@gmail.com>
2018-04-25 15:45:06 +0100

    project structure created

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import {Request, Response, NextFunction, Application} from 'express';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as path from 'path';
+import DbConnection from './database/DbConnection'
+
+// Creates and configures an ExpressJS web server.
+export class App {
+
+    public express: Application;
+
+    //Run configuration methods on the Express instance.
+    constructor() {
+        this.express = express();
+        this.express.use('/', express.static(path.join(__dirname, '../frontend/dist')));
+        this.middleware();
+        this.routes();
+        // this.cloudinaryConfig();
+
+        DbConnection.connect();
+        DbConnection.errorHandler();
+
+    }
+
+    // Configure Express middleware.
+    private middleware(): void {
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({extended: false}));
+    }
+
+    // Configure API endpoints.
+    private routes(): void {
+
+        this.express.use('*', (req: Request, res: Response, next: NextFunction) => {
+            res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+        });
+
+
+    }
+
+    // private cloudinaryConfig(): void {
+    //     cloudinary.config({
+    //         cloud_name: 'dj5hg3jvo',
+    //         api_key: '465423256943594',
+    //         api_secret: '7QHalxaRHtG4oU-KGfr5X7-ztfc'
+    //     });
+    //
+    //
+    // }
+
+}
+
+export default new App().express;
---
+++ src/database/dbConnection.ts
@@ -0,0 +1,45 @@
+import * as mongoose from 'mongoose';
+
+
+class DbConnection {
+
+    public db: mongoose.Mongoose;
+
+    private database = {
+        dbUser: 'admin',
+        dbPassword: 'admin',
+        dbServer: 'ds038319.mlab.com:38319',
+        dbName: 'cms-internet'
+    };
+
+    public connect(): void {
+        const options = {
+            autoReconnect: true,
+            reconnectInterval: 1000,
+            keepAlive: 1500,
+            reconnectTries: Number.MAX_VALUE
+        };
+        mongoose.connect('mongodb://' + this.database.dbUser + ':' + this.database.dbPassword + '@' + this.database.dbServer + '/' + this.database.dbName, options);
+        this.db = mongoose;
+    }
+
+    public errorHandler(): void {
+        mongoose.connection.on('error', () => {
+            console.log('Server is unreachable.');
+        });
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected');
+        });
+        mongoose.connection.on("reconnected", () => {
+            console.log("Reconnected to MongoDB");
+        });
+        mongoose.connection.on('connected', () => {
+            console.log('MongoDB connected.');
+        });
+        mongoose.connection.once('open', () => {
+            console.log('MongoDB connection opened.');
+        });
+    }
+}
+
+export default new DbConnection();
